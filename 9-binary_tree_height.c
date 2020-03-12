071afb0dc23ef98c4b4d1774e4683bf11bc36575
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-12 01:16:18 +0000

    requirement check and Edge case size_t fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,22 @@
 #include "binary_trees.h"
+/**
+  *binary_max - measures node count in longest path from root to leaf
+  *@tree: pointer to root node of binary tree structure
+  *Return: H or 0
+  */
 size_t binary_max(const binary_tree_t *tree)
 {
-	size_t left_child, right_child, H = 1;	
+	size_t left_child, right_child, H;
 
 	if (tree == NULL)
 		return (0);
 	left_child = binary_max(tree->left);
 	right_child = binary_max(tree->right);
 	if (left_child > right_child)
-		H = H + left_child;
+		H = 1 + left_child;
 	else
-		H = H + right_child;
-	return H;
+		H = 1 + right_child;
+	return (H);
 }
 /**
   *binary_tree_height - measures the height of Binary tree from node
@@ -23,5 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t holb;
 
 	holb = binary_max(tree);
-	return holb - 1;
+	if (holb == 0)
+		return (0);
+	return (holb - 1);
 }
