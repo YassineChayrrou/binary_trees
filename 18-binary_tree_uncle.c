7accfbbdc5ac050060014d8b90dfba4a39f85b0a
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-13 03:27:08 +0000

    binary tree uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+  *binary_tree_uncle - checks if BT node has an uncle
+  *@node: pointer to a node of Binary Tree structure
+  *Return: pointer to uncle or NULL
+  */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+	if (node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+	return (NULL);
+}
